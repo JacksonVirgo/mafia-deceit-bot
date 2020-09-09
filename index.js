df338b342394e9f9035969421f2dd767c0eab0e8
JacksonVirgo <jacknash42@gmail.com>
2020-09-10 07:08:21 +1000

    Config.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = process.env.config || require('./config.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log(client.user.username + " is online");
